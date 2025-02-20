ad82b6db0b232621c601f9a74cdeeb580657c580
Angela Lima <angelamcostalima@icloud.com>
2025-02-20 16:26:22 +0000

    ✨ add simple `fastify` server

--- srcs/server/tools/server.mjs
+++ srcs/server/tools/server.mjs
@@ -1,19 +1,14 @@
-// * This code is from https://fastify.dev being currently used for testing purposes
-
-// Import the framework and instantiate it
 import Fastify from 'fastify/fastify.js'
 const fastify = Fastify({
 	logger: true
 })
 
-// Declare a route
 fastify.get('/', async function handler(request, reply) {
 	return { hello: 'world' }
 })
 
-// Run the server!
 try {
-	await fastify.listen({ port: 3000 })
+	await fastify.listen({ port: 3000, host: '0.0.0.0' })
 } catch (err) {
 	fastify.log.error(err)
 	process.exit(1)
