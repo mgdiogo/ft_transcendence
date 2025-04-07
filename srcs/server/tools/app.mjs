a610ea4f64e4cb3c85afa591633e4d07b252014d
Angela Lima <angelamcostalima@icloud.com>
2025-04-07 15:38:21 +0100

    🎉 add `server` skeleton

---
+++ srcs/server/tools/app.mjs
@@ -0,0 +1,15 @@
+import Fastify from 'fastify/fastify.js'
+const fastify = Fastify({
+	logger: true
+})
+
+fastify.get('/', async function handler(request, reply) {
+	return { hello: 'world' }
+})
+
+try {
+	await fastify.listen({ port: 9000, host: '0.0.0.0' })
+} catch (err) {
+	fastify.log.error(err)
+	process.exit(1)
+}
